0bb09973673a0e19445186963313d9c979b0cd89
sardinecan <morvan.josselin@gmail.com>
2016-11-24 10:00:26 +0100

    nettoyage

--- JS/accordion_equalizer/reflow.js
+++
@@ -1,3 +0,0 @@
-$('.accordion').on('toggled', function (event, accordion) {
-                    $(document).foundation('equalizer', 'reflow');
-                    })
--- JS/menu/long_dropdown.js
+++
@@ -1,7 +0,0 @@
-function fixDropdown() {   
-/*pour l'accès rapide par expéditeur : cible les ul drop de troisième niveau. sinon empêche l'affichage si le deuxième niveau est ciblé.*/
-    var dropdown = $('ul.dropdown ul.dropdown ul.dropdown');
-    dropdown.css({ 'max-height': 300, 'overflow-y': 'auto' });
-}
-$(window).on('load', fixDropdown);
-$(window).on('resize', fixDropdown);
